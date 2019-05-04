2da107c811dafbf9eb356ac957059a36434ac6b6
PCVM\Geoff <josper0544@gmail.com>
2019-05-05 00:05:12 +1000

    Added react-icons to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <p>We are at the point of including react-icons to the index page</p>
     <FaGulp />
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
