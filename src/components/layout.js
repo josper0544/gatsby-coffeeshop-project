2dd8aed179e276ce990bf7d83da3e574bab475a1
PCVM\Geoff <josper0544@gmail.com>
2019-05-04 22:28:32 +1000

    first commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+
+import "./bootstrap.min.css"
 import "./layout.css"
 
 const Layout = ({ children }) => (
@@ -25,7 +26,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        
         <div
           style={{
             margin: `0 auto`,
