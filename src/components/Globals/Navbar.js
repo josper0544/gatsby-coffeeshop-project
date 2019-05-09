91568f4cdfadb6de053bafd2b526a87775fe7cf5
PCVM\Geoff <josper0544@gmail.com>
2019-05-09 15:46:40 +1000

    Navbar added and css changed

---
+++ src/components/Globals/Navbar.js
@@ -0,0 +1,68 @@
+  import React, { Component } from 'react';
+import {Link} from "gatsby";
+import Logo from "../../images/Logo.svg";
+import {FaCartArrowDown} from "react-icons/fa";
+
+// The Navbar Component stores the Navbar item data in the state property
+
+export default class Navbar extends Component {
+    state={
+        navbarOpen: false,
+        css:'collapse navbar-collapse',
+        links:[
+            {
+                id:1,
+                path:'/',
+                text:'home'
+            },
+            {
+                id:2,
+                path:'/about',
+                text:'about'
+            },
+            {
+                id:3,
+                path:'/specials',
+                text:'specials'
+            }
+        ]
+    };
+    navbarHandler = () => {
+        this.state.navbarOpen? this.setState(
+            {navbarOpen:false,
+            css: "collapse navbar-collapse"})
+            : this.setState(
+            {navbarOpen: true,
+            css: "collapse navbar-collapse show"});
+        
+    };
+  render() {
+    return (
+      <nav className = "navbar navbar-expand-sm bg-light navbar-light">
+          <Link to="/" className = "navbar-brand">
+            <img src = {Logo} alt="logo" />
+            {/* attribution text */}
+          </Link>
+          <button  className = "navbar-toggler" type = "button" onClick = {this.navbarHandler}>
+            <span className = "navbar-toggler-icon"></span>
+          </button>
+          <div className = {this.state.css}>
+              <ul className="navbar-nav mx-auto">
+                  {this.state.links.map(link => {
+                      return(
+                          <li key = {link.id} className = "nav-item">
+                              <Link to={link.path} className = "nav-link text-capitalize">
+                                {link.text}
+                              </Link>
+                          </li>
+                      );
+                  })}
+              </ul>
+              <li className="nav-item ml-sm-5">
+                  <FaCartArrowDown className="cart-icon" />
+              </li>
+          </div>
+      </nav>
+    );
+  }
+};
--- src/components/layout.js
+++ src/components/layout.js
@@ -12,6 +12,7 @@ import { StaticQuery, graphql } from "gatsby"
 
 import "./bootstrap.min.css"
 import "./layout.css"
+import Navbar from "./Globals/Navbar"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -27,21 +28,14 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <Navbar />
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </footer>
-        </div>
+        
       </>
     )}
   />
